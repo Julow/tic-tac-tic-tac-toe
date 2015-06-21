9a39aae590130dc84a4181d70c07d6456aa5938e
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-21 11:13:55 +0200

    New IA almost done

--- srcs/IA.ml
+++ srcs/IA.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/21 09:20:57 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/21 10:13:44 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/21 11:13:38 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -17,19 +17,64 @@ let sbindex = [0; 1; 2; 3; 4; 5; 6; 7; 8]
 
 let empty_moves = [[]; []; []; []; []]
 
+(** sbi: subboard index
+ ** ai,bi,ci: triplet indexes unpacked
+ ** tv: triplet values*)
+let any_no_coord sbi (ai, bi, ci) tv =
+  match tv with
+  | (Owner.No, _, _)			-> MainBoard.getcell_xy sbi ai
+  | (_, Owner.No, _)			-> MainBoard.getcell_xy sbi bi
+  | (_, _, Owner.No)			-> MainBoard.getcell_xy sbi ci
+  | _							-> failwith "Unreachable!!"
+											
+(** lm: list matches
+ ** lm: list matches index
+ ** mcoo: match coordinates (int ^3) *)
+let push_match lm lmi mcoo =
+  let rec helper lm lmi' =
+	match lm with
+	| []							-> []
+	| hd::tl when lmi' = lmi		-> (mcoo::hd)::(helper tl (lmi' + 1))
+	| hd::tl						-> hd::(helper tl (lmi' + 1))
+  in
+  helper lm 0
 
-(** ti: triplet indexes (int * int * int)
- ** ai,bi,ci: tripled indexes unpacked
+(** ti: triplet indexes (int ^3)
+ ** tv: triplet values (Owner.t ^3)
+ ** nno,npi,np2: triplet values summed
  ** cl: cell list
  ** sbi: subboard index
  ** lm: list matches
  **
  ** **)
-let foreach_triplets ((ai, bi, ci) as ti) cl sbi lm =
-  let (av, bv, cv) = Board.getTripletValues cl ti in
-  lm
+let foreach_triplets ti cl sbi lm =
+  (*to remove*)  let push = lm in (* DEBUG *)
+  let tv = Board.getTripletValues cl ti in
+  let (nno, np1, np2) = Owner.sumOwners tv in
+  if nno = 1 && np1 = 2 then
+	push
+  else if List.length (List.nth lm 0) = 0 then
+	begin
+	  if nno = 1 && np2 = 2 then
+		push_match lm 1 (any_no_coord sbi ti tv)
+	  else if List.length (List.nth lm 1) = 0 then
+		begin
+		  if nno = 2 && np2 = 1 then
+			push
+		  else
+			lm
+		end
+	  else
+		lm
+	end
+  else
+	lm
 
-
+(** cl: cell list
+ ** sbi: subboard index
+ ** lm: matches list
+ ** tl: triplet list
+ ** f: foreach_triplet[_diag] function *)
 let check_triplets cl sbi lm =
   let rec helper tl f lm =
 	match tl with
--- srcs/Owner.ml
+++ srcs/Owner.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/20 11:30:02 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/20 17:15:18 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/21 10:29:32 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -16,11 +16,21 @@
 type t = No | P1 | P2
 
 let next = function
-	| P1			-> P2
-	| P2			-> P1
-	| No			-> No
+  | P1			-> P2
+  | P2			-> P1
+  | No			-> No
 
 let char_of_owner = function
   | No					-> 'N'
   | P1					-> 'O'
   | P2					-> 'X'
+
+let sumOwners (a, b, c) =
+  let rec helper l ((sa, sb, sc) as t) =
+	match l with
+	| []					-> t
+	| No::tl				-> helper tl (sa + 1, sb, sc)
+	| P1::tl				-> helper tl (sa, sb + 1, sc)
+	| P2::tl				-> helper tl (sa, sb, sc + 1)
+  in
+  helper [a; b; c] (0, 0, 0)
